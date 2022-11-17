d7c46482f3228ba4b6cb97277d1ed9c9795540d8
RelictHominid <111104572+RelictHominid@users.noreply.github.com>
2022-11-17 13:10:10 +1300

    created food order menu and price calculation

--- SchoolLunchApp/Main.cpp
+++ SchoolLunchApp/Main.cpp
@@ -124,6 +124,363 @@ void createMainMenu() {
 }
 
 void createFoodMenu() {
+	//Menu Variables
+	char pizza1[] = "Cheese", pizza2[] = "Meatlovers", pizza3[] = "Supreme";
+	char drinks1[] = "Sprite", drinks2[] = "Coke", drinks3[] = "Orange Juice";
+	char burger1[] = "Cheese Burger", burger2[] = "Chicken Burger", burger3[] = "Fish Burger";
+	char sandwich1[] = "Ham & Cheese", sandwich2[] = "Tuna & Mayo", sandwich3[] = "Meatball";
+	char fried1[] = "Chicken Nuggets", fried2[] = "French Fries", fried3[] = "Onion Rings";
+
+	char gotoBeginning;
+	int foodOption;
+	double qty, option = 0, pizzaPrice = 0.00, drinkPrice = 0.00, burgerPrice = 0.00, sandwichPrice = 0.00, friedPrice = 0.00;
+
+
+start:
+	system("cls");
+
+	// Main Food Menu
+	cout << "\n\nWhat would you like to order?\n\n";
+
+
+	cout << "[1] Pizzas\n";
+	cout << "[2] Burgers\n";
+	cout << "[3] Sandwich\n";
+	cout << "[4] Drinks\n";
+	cout << "[5] Fried Food\n";
+	cout << "[6] Display Total Bill\n";
+	cout << "\nPlease Enter your Choice: ";
+	cin >> option;
+
+	// Pizza Options
+	if (option == 1)
+	{
+		cout << "\n1) " << pizza1 << "\n";
+		cout << "2) " << pizza2 << "\n";
+		cout << "3) " << pizza3 << "\n";
+		cout << "\nWhat type of pizza would you like?:";
+		cin >> foodOption;
+
+		// Pizza Size, Quantity & Price
+		if (foodOption >= 1 && foodOption <= 5)
+		{
+			cout << "\n1) Small $5.50\n" << "2) Regular $15.25\n" << "3) Large $20.75\n";
+			cout << "\nWhat size would you like?:";
+			cin >> foodOption;
+			if (foodOption >= 1 && foodOption <= 3)
+				cout << "\nHow many would you like to order?: ";
+			cin >> qty;
+			switch (foodOption)
+			{
+			case 1: pizzaPrice = 5.50 * qty;
+				break;
+
+			case 2: pizzaPrice = 15.25 * qty;
+				break;
+
+			case 3: pizzaPrice = 20.75 * qty;
+				break;
+
+
+			}
+			system("cls");
+			switch (foodOption)
+			{
+			case 1:
+				cout << "\t\t\t--------Your Order---------\n";
+				cout << "" << qty << " x " << pizza1 << " Pizza";
+				cout << "\nYour Total Bill is: $" << pizzaPrice << endl;
+				break;
+			case 2:
+				cout << "\t\t--------Your Order---------\n";
+				cout << "" << qty << " x " << pizza2 << " Pizza";
+				cout << "\nYour Total Bill is: $" << pizzaPrice << endl;
+				break;
+			case 3:
+				cout << "\t\t--------Your Order---------\n";
+				cout << "" << qty << " x " << pizza3 << " Pizza";
+				cout << "\nYour Total Bill is: $" << pizzaPrice << endl;
+				break;
+
+			}
+			cout << "Would you like to order anything else?";
+			cout << "\nPress Y to return back to menu: ";
+			cin >> gotoBeginning;
+			if (gotoBeginning == 'Y' || gotoBeginning == 'y')
+			{
+				goto start;
+
+			}
+
+
+
+		}
+
+	}
+
+	// Burger Options
+	else if (option == 2)
+	{
+		cout << "\n1) " << burger1 << " $18.00" << "\n";
+		cout << "2) " << burger2 << " $15.00" << "\n";
+		cout << "3) " << burger3 << " $16.00" << "\n";
+		cout << "\nWhat type of Burger you would like to have?: ";
+		cin >> foodOption;
+
+		// Burger Quantity & Price
+		if (foodOption >= 1 && foodOption <= 3)
+		{
+			cout << "\nPlease Enter Quantity: ";
+			cin >> qty;
+			switch (foodOption)
+			{
+			case 1: burgerPrice = 18.00 * qty;
+				break;
+
+			case 2: burgerPrice = 15.00 * qty;
+				break;
+
+			case 3: burgerPrice = 16.00 * qty;
+				break;
+
+			}
+			system("cls");
+			switch (foodOption)
+			{
+			case 1:
+				cout << "\t\t--------Your Order---------\n";
+				cout << "" << qty << " x " << burger1;
+				cout << "\nYour Total Bill is: $" << burgerPrice << endl;
+				break;
+			case 2:
+				cout << "\t\t--------Your Order---------\n";
+				cout << "" << qty << " x " << burger2;
+				cout << "\nYour Total Bill is: $" << burgerPrice << endl;
+				break;
+			case 3:
+				cout << "\t\t--------Your Order---------\n";
+				cout << "" << qty << " x " << burger3;
+				cout << "\nYour Total Bill is: $" << burgerPrice << endl;
+				break;
+
+
+			}
+			cout << "\nWould you like to order anything else?";
+			cout << "\nPress Y to return back to menu: ";
+			cin >> gotoBeginning;
+			if (gotoBeginning == 'Y' || gotoBeginning == 'y')
+			{
+				goto start;
+
+			}
+
+		}
+	}
+
+	// Sandwich Options
+	else if (option == 3)
+	{
+		cout << "\n1)  " << sandwich1 << " $24.00" << "\n";
+		cout << "2)  " << sandwich2 << " $16.00" << "\n";
+		cout << "3)  " << sandwich3 << " $10.00" << "\n";
+		cout << "\nWhat type Sandwich you would like to have?:";
+		cin >> foodOption;
+
+		// Sandwich Quantity & Price
+		if (foodOption >= 1 && foodOption <= 3)
+		{
+			cout << "\nPlease Enter Quantity: ";
+			cin >> qty;
+			switch (foodOption)
+			{
+			case 1: sandwichPrice = 24.00 * qty;
+				break;
+
+			case 2: sandwichPrice = 16.00 * qty;
+				break;
+
+			case 3: sandwichPrice = 10.00 * qty;
+				break;
+
+			}
+			system("cls");
+			switch (foodOption)
+			{
+			case 1:
+				cout << "\t\t--------Your Order---------\n";
+				cout << "" << qty << " x " << sandwich1;
+				cout << "\nYour Total Bill is : $" << sandwichPrice << endl;
+				break;
+			case 2:
+				cout << "\t\t--------Your Order---------\n";
+				cout << "" << qty << " x " << sandwich2;
+				cout << "\nYour Total Bill is : $" << sandwichPrice << endl;
+				break;
+			case 3:
+				cout << "\t\t--------Your Order---------\n";
+				cout << "" << qty << " x " << sandwich2;
+				cout << "\nYour Total Bill is : $" << sandwichPrice << endl;
+				break;
+
+
+			}
+			cout << "Would you like to order anything else?";
+			cout << "\nPress Y to return back to menu: ";
+			cin >> gotoBeginning;
+			if (gotoBeginning == 'Y' || gotoBeginning == 'y')
+			{
+				goto start;
+
+			}
+		}
+	}
+
+	// Drink Options
+	else if (option == 4)
+	{
+		cout << "\n1) " << drinks1 << " $3.00" << "\n";
+		cout << "2) " << drinks2 << " $3.00" << "\n";
+		cout << "3) " << drinks3 << " $4.00" << "\n";
+		cout << "\nWhat type of drink you would like to have?: ";
+		cin >> foodOption;
+
+		// Drink Quantity & Price
+		if (foodOption >= 1 && foodOption <= 3)
+		{
+			cout << "\nHow many drinks would you like?: ";
+			cin >> qty;
+			switch (foodOption)
+			{
+			case 1: drinkPrice = 3.00 * qty;
+				break;
+
+			case 2: drinkPrice = 3.00 * qty;
+				break;
+
+			case 3: drinkPrice = 4.00 * qty;
+				break;
+
+			}
+			system("cls");
+			switch (foodOption)
+			{
+			case 1:
+				cout << "\t\t--------Your Order---------\n";
+				cout << "" << qty << " x " << drinks1;
+				cout << "\nYour Total Bill is : $" << drinkPrice << endl;
+				break;
+			case 2:
+				cout << "\t\t--------Your Order---------\n";
+				cout << "" << qty << " x " << drinks2;
+				cout << "\nYour Total Bill is : $" << drinkPrice << endl;
+				break;
+			case 3:
+				cout << "\t\t--------Your Order---------\n";
+				cout << "" << qty << " x " << drinks3;
+				cout << "\nYour Total Bill is : $" << drinkPrice << endl;
+				break;
+
+
+			}
+			cout << "Would you like to order anything else?";
+			cout << "\nPress Y to return back to menu: ";
+			cin >> gotoBeginning;
+			if (gotoBeginning == 'Y' || gotoBeginning == 'y')
+			{
+				goto start;
+
+			}
+		}
+	}
+
+	// Fried Food Options
+	else if (option == 5)
+	{
+		cout << "\n1) " << fried1 << " $11.00" << "\n";
+		cout << "2) " << fried2 << " $7.50" << "\n";
+		cout << "3) " << fried3 << " $10.25" << "\n";
+		cout << "\nPlease Enter which Fried you would like to have?:";
+		cin >> foodOption;
+
+		// Fried Food Quantity & Price
+		if (foodOption >= 1 && foodOption <= 3)
+		{
+			cout << "\nPlease Enter Quantity: ";
+			cin >> qty;
+			switch (foodOption)
+			{
+			case 1: friedPrice = 11.00 * qty;
+				break;
+
+			case 2: friedPrice = 7.50 * qty;
+				break;
+
+			case 3: friedPrice = 10.25 * qty;
+				break;
+
+			}
+			system("cls");
+			switch (foodOption)
+			{
+			case 1:
+				cout << "\t\t--------Your Order---------\n";
+				cout << "" << qty << " x " << fried1;
+				cout << "\nYour Total Bill is : $" << friedPrice << endl;
+				break;
+			case 2:
+				cout << "\t\t--------Your Order---------\n";
+				cout << "" << qty << " x " << fried2;
+				cout << "\nYour Total Bill is : $" << friedPrice << endl;
+				break;
+			case 3:
+				cout << "\t\t--------Your Order---------\n";
+				cout << "" << qty << " x " << fried3;
+				cout << "\nYour Total Bill is : $" << friedPrice << endl;
+				break;
+
+
+			}
+			cout << "Would you like to order anything else?";
+			cout << "\nPress Y to return back to menu: ";
+			cin >> gotoBeginning;
+			if (gotoBeginning == 'Y' || gotoBeginning == 'y')
+			{
+				goto start;
+
+			}
+		}
+	}
+
+
+	else if (option == 6) {
+
+		double totalCost = pizzaPrice + drinkPrice + burgerPrice + sandwichPrice + friedPrice;
+
+		system("cls");
+
+		cout << "Total order amount is: $" << totalCost << endl;
+		cout << "\nPress Y to return back to menu: ";
+		cin >> gotoBeginning;
+
+		if (gotoBeginning == 'Y' || gotoBeginning == 'y')
+		{
+			goto start;
+			return totalCost;
+		}
+	}
+	else
+	{
+		system("cls");
+		cout << "Invaild Selection, please choose from the options available. \n";
+		cout << "Would You like to try to order again? Y / N: ";
+		cin >> gotoBeginning;
+
+		if (gotoBeginning == 'Y' || gotoBeginning == 'y')
+		{
+			goto start;
+
+		}
+	}
 
 }
 
